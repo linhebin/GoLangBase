8affd24378e22d00707b77cc86a867d4fc2a67b2
linhebin <linhebin@qiyi.com>
2019-05-02 18:28:00 +0800

    学习

--- unit01/oneOne.go
+++ unit01/oneOne.go
@@ -13,9 +13,9 @@ func sum(values []int, resultChan chan int)  {
 func main()  {
     values := [] int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
     fmt.Println(values)
-    resultChan := make(chan int, 2)
-    go sum(values[:len(values)/2], resultChan)
-    go sum(values[len(values)/2:], resultChan)
+    resultChan := make(chan int, 2)  // 建一个两个值的管道
+    go sum(values[:len(values)/2], resultChan)  // 起一个协程
+    go sum(values[len(values)/2:], resultChan)  // 起一个协程
     sum1, sum2 := <- resultChan, <- resultChan // 接收结果
     fmt.Println("Result:",  sum1, sum2, sum1 + sum2)
 }
--- unit01/oneTwo.go
+++ unit01/oneTwo.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+    "fmt"
+    "reflect"
+)
+
 type Bird struct {
     Name string
     LifeExpectance int
+}
+
+func (b Bird) Fly()  {
+    fmt.Println("I am flying...")
+}
+
+func main()  {
+    sparrow := &Bird{"Sparrow", 3}  // 初始化一结构体
+    s := reflect.ValueOf(sparrow).Elem() // 反射， 得到对像
+    typeOft := s.Type()
+    for i := 0; i < s.NumField(); i ++ {
+        f := s.Field(i)
+        fmt.Println(fmt.Sprintf("%d: %s %s = %v\n", i, typeOft.Field(i).Name, f.Type(), f.Interface()))
+    }
 }
